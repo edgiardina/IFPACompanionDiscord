eb189d768f8e9cfde499bc41f7ca856f0f429b82
Ed Giardina <ed@edgiardina.com>
2022-01-19 21:32:21 -0500

    added string extensions

---
+++ IFPACompanionDiscord/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFPACompanionDiscord.Extensions
+{
+    internal static class StringExtensions
+    {
+        public static string RemoveCharactersAfterLastOccurrence(this string s, char c)
+        {
+            int idx = s.LastIndexOf(c);
+
+            if (idx != -1)
+            {
+                return s[..idx];
+            }
+            else return s;
+        }
+
+    }
+}
